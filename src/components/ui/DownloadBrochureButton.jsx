1af26dd0b269bb7dac2efb8a601fdc936a0c9049
MishtiGarg250 <msihti811@gmail.com>
2025-12-10 15:25:53 +0530

    fix: remove component error

--- src/components/ui/DownloadBrochureButton.jsx
+++ src/components/ui/DownloadBrochureButton.jsx
@@ -1,5 +1,5 @@
 'use client';
-import { Button } from '@/components/ui/Button';
+import { Button } from '@/components/ui/Button.jsx';
 import { Download } from 'lucide-react';
 
 export default function DownloadBrochureButton({
